6da8b21e55a2b351e7dca3e3bd2a0c39fcef3ad8
adosib <adosib1@gmail.com>
2025-04-16 12:14:56 -0500

    Support reading secrets from local or cloud

--- indeed_client.py
+++ indeed_client.py
@@ -4,14 +4,40 @@ import logging
 from pathlib import Path
 
 import requests
-from dotenv import load_dotenv
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-load_dotenv()
+
+
+def is_local_env():
+    return os.getenv("ENV", "local").lower() == "local"
+
+
+def access_secret_version(secret_id):
+    from google.cloud import secretmanager_v1
+
+    client = secretmanager_v1.SecretManagerServiceClient()
+    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+
+def load_secrets():
+    if is_local_env():
+        from dotenv import load_dotenv
+
+        load_dotenv()
+    else:
+        secret_names = ["BRD_AUTH_TOKEN", "GCP_BRD_SA_KEY"]
+        for secret_name in secret_names:
+            os.environ[secret_name] = access_secret_version(secret_name)
+
+
+load_secrets()
 
 ROOT = Path(__file__).parent
 BRD_URL = "https://api.brightdata.com/datasets/v3/trigger"
